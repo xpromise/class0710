72a21c54455e18119af38697478ba55a6e9cf48f
xiongjian <webjsforyou@gmail.com>
2018-09-29 14:52:28 +0800

    包和包管理器

---
+++ 02_nodejs/day01/05_包.js
@@ -0,0 +1,15 @@
+/*
+  通过npm能去下载，搜索、删除包
+    1. npm install/i xxx  ===  npm i xxx --save  === npm i xxx -S
+      - 在当前目录下创建node_modules文件夹，将包下载到文件夹中
+      - 在当前目录下创建package-lock.json文件，它是用来做缓存，让后面的下载更快
+      - 自动添加到package.json中的生产依赖中
+   2. npm i xxx --save-dev === npm i xxx -D
+    - 自动添加到package.json中的开发依赖中
+   3. npm i
+    - 自动将package.json所有的依赖（开发和生产）全部下载下来
+   4. npm remove/r xxx
+    - 删除包和相关的依赖
+   5. npm i xxx -g
+    - 全局安装一个包(可以在cmd中运行指令)
+ */
