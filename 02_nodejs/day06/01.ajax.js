9022c78f6211a32d6d1e92a9b9b48fe251b9450a
xiongjian <webjsforyou@gmail.com>
2018-10-12 11:03:55 +0800

    ajax解析

--- 02_nodejs/day06/01.ajax.js
+++ 02_nodejs/day06/01.ajax.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static('public'));
@@ -7,6 +8,26 @@ app.get('/testAjax', function (req, res) {
   console.log(req.query);
   res.send('这是服务器返回的响应');
 })
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/testAjax', bodyParser.text(), function (req, res) {
+  /*
+    Content-Type: text/plain;charset=UTF-8
+    解析： bodyParser.text()
+    Content-Type: application/x-www-form-urlencoded
+    解析： bodyParser.urlencoded()
+    Content-Type: application/json
+    解析： bodyParser.json()
+    如果是流
+    解析：bodyParser.raw()
+   */
+  console.log('*********');
+  console.log(req.body);  // {"username":"sunwukong","pwd":123456}
+  res.send('这是服务器返回的响应');
+})
+
+
 
 app.listen(3000, function (err) {
   if (!err) console.log('服务器启动成功了~');
