ba1f50d410508af4067a3144ef7e85426d3e403c
xiongjian <webjsforyou@gmail.com>
2018-10-12 15:59:31 +0800

    跨域 - jsonp和cors

--- 02_nodejs/day06/01.ajax.js
+++ 02_nodejs/day06/01.ajax.js
@@ -27,6 +27,36 @@ app.post('/testAjax', bodyParser.text(), function (req, res) {
   res.send('这是服务器返回的响应');
 })
 
+app.get('/testJSONP', function (req, res) {
+  //获取请求参数
+  var callback = req.query.callback;
+  //去数据库中查找数据
+  var data = {
+    username: 'sunwukong',
+    age: 18
+  }
+  //jsonp  json with padding
+  console.log(callback + '(' + JSON.stringify(data) + ')');
+  //getData({"username":"sunwukong","age":18})
+  //返回响应
+  res.send(callback + '(' + JSON.stringify(data) + ')');
+
+})
+
+app.get('/testCors', function (req, res) {
+  /*
+    cors：官方提供的解决方案
+      特点：
+        1. 兼容性较差
+        2. 能够满足所有的请求方式
+   */
+  
+  //设置响应头
+  // res.set('Access-Control-Allow-Origin', '*');  //允许所有网址跨域
+  res.set('Access-Control-Allow-Origin', 'http://localhost:63342');
+  res.send('这是cors返回的响应');
+})
+
 app.listen(3000, function (err) {
   if (!err) console.log('服务器启动成功了~');
   else console.log(err);
