3ae689a1c1f1d14aa01ff3c861deb4b80a5299c0
xiongjian <webjsforyou@gmail.com>
2018-10-13 09:34:31 +0800

    jquery的ajax练习完成

--- 02_nodejs/day06/06.testAjax02/index.js
+++ 02_nodejs/day06/06.testAjax02/index.js
@@ -3,6 +3,8 @@ require('./db');
 var Cities = require('./model/cities');
 var app = express();
 
+app.use(express.static('public'));
+
 //获取省份信息
 app.get('/getProvince', function (req, res) {
   //去数据库中查询所有省份的数据
@@ -33,7 +35,21 @@ app.get('/getCity', function (req, res) {
 })
 
 //获取区县信息
-
+app.get('/getCounty', function (req, res) {
+  //获取省份信息
+  var province = req.query.province;
+  //获取市信息
+  var city = req.query.city;
+  //去数据库中查询所有区县的数据
+  Cities.find({level: 3, province: province, city: city}, {name: 1, county: 1, _id: 0}, function (err, data) {
+    if (!err && data.length) {
+      //找到了数据并且方法没有出错, 返回响应
+      res.json({status: '1', data: data});
+    } else {
+      res.json({status: '0'});
+    }
+  })
+})
 
 app.listen(3000, function (err) {
   if (!err) console.log('服务器启动成功了');
